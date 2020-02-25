add98be5d5467f60013d893fed9e771495cd7a84
Paolo <pikinor24@yahoo.it>
2020-02-25 13:15:27 +0100

    paolo: test adc_reader

--- server_atmega/adc_reader.c
+++ server_atmega/adc_reader.c
@@ -17,9 +17,11 @@ int main(int argc, char** argv) {
   printf_init();
   int v = 0;
   uint8_t pin = 1;
+  int it = 0;
   while(1){
+    it++;
     v = get_digital_value(pin);
-    printf("value: %u\n", v);
+    printf("%d value: %u\n", it, v);
     _delay_ms(100);
   }
   return 0;
@@ -27,29 +29,31 @@ int main(int argc, char** argv) {
 
 int get_digital_value(uint8_t pin)
 {
-  //paolo, test dell'adc su singolo pin
   uint8_t low, high;
-  
-  // imposta MUX5 a 1 in caso si vogliano leggere i pin A8-A15
+
+  // the MUX5 bit of ADCSRB selects whether we're reading from channels
+  // 0 to 7 (MUX5 low) or 8 to 15 (MUX5 high).
   ADCSRB = (ADCSRB & ~(1 << MUX5)) | (((pin >> 3) & 0x01) << MUX5);
+  
+  // set the analog reference (high two bits of ADMUX) and select the
+  // channel (low 4 bits).  this also sets ADLAR (left-adjust result)
+  // to 0 (the default).
+  ADMUX = (1 << 6) | (pin & 0x07);
 
-  // imposto il canale corrispondente al pin (bit3:0 di ADMUX) mentre
-  //imposto ADLAR REFS1 REFS0 a 0 in modo d avere Vcc come riferimento
-  // ed il risultato della conversione right-adjusted
-  ADMUX = (pin & 0x07);
-
-  //imposto ad 1 ADSC per iniziare una conversione
-  ADCSRA |= 1<<ADSC;
-
-  // busy-waiting su ADSC per aspettare la fine conversione
-  while (ADCSRA | 1<<ADSC);
-
-  // la lettura di ADCL blocca qualsiasi converione fino a che
-  //non venga letto ADCH, se leggessi prima ADCH l'altro registro
-  //verrebbe scartato
+  // start the conversion
+  ADCSRA |= (1 << ADSC);
+  // ADSC is cleared when the conversion finishes
+  while (ADCSRA & (1 << ADSC)){
+    printf("waiting\n");
+  };
+  printf("done waiting\n");
+  // we have to read ADCL first; doing so locks both ADCL
+  // and ADCH until ADCH is read.  reading ADCL second would
+  // cause the results of each conversion to be discarded,
+  // as ADCL and ADCH would be locked when it completed.
   low  = ADCL;
   high = ADCH;
 
-  // combino i due byte per avre 10-bit di precisione
+  // combine the two bytes
   return (high << 8) | low;
 }
