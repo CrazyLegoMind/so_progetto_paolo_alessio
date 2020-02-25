d8c20206d764da7cd145cf632c5a0fa1f9d936ee
Paolo <pikinor24@yahoo.it>
2020-02-25 15:39:03 +0100

    PDGZ: finito lettura pin adc

--- server_atmega/adc_reader.c
+++ server_atmega/adc_reader.c
@@ -12,11 +12,9 @@ uint8_t in_pin;
 
 int main(int argc, char** argv) {
 
-  DDRF &= 0x00;
-  PORTF != 0xFF;
   printf_init();
   int v = 0;
-  uint8_t pin = 1;
+  uint8_t pin = 0;
   int it = 0;
   while(1){
     it++;
@@ -27,26 +25,30 @@ int main(int argc, char** argv) {
   return 0;
 }
 
-int get_digital_value(uint8_t pin)
-{
+int get_digital_value(uint8_t pin){
   uint8_t low, high;
+
+  DIDR0 = 0x00;           //Digital input disabled on all ADC ports
+  PRR0 &= ~(1<<PRADC);    //Enabled adc on the power reduction register
+
+  ADCSRA = 1 << ADEN; //enabling ADC itself
 
   // the MUX5 bit of ADCSRB selects whether we're reading from channels
   // 0 to 7 (MUX5 low) or 8 to 15 (MUX5 high).
+
   ADCSRB = (ADCSRB & ~(1 << MUX5)) | (((pin >> 3) & 0x01) << MUX5);
   
   // set the analog reference (high two bits of ADMUX) and select the
   // channel (low 4 bits).  this also sets ADLAR (left-adjust result)
   // to 0 (the default).
+  
   ADMUX = (1 << 6) | (pin & 0x07);
 
   // start the conversion
   ADCSRA |= (1 << ADSC);
   // ADSC is cleared when the conversion finishes
-  while (ADCSRA & (1 << ADSC)){
-    printf("waiting\n");
-  };
-  printf("done waiting\n");
+  while (ADCSRA & (1 << ADSC));
+  
   // we have to read ADCL first; doing so locks both ADCL
   // and ADCH until ADCH is read.  reading ADCL second would
   // cause the results of each conversion to be discarded,
