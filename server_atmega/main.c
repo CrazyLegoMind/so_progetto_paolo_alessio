4f14614f3603033236974eea7fab5e7ad165deed
Paolo <pikinor24@yahoo.it>
2020-08-28 12:37:44 +0200

    PDGZ: starting temp main to test serial

--- server_atmega/main.c
+++ server_atmega/main.c
@@ -3,6 +3,20 @@
 #include "../common_lib/serial_utils.h"
 
 
+#define RECEIVE_AMOUNT 5
+#define SEND_AMOUNT 10
+UART uart_ptr;
+
 int main(char agrc, char* argv){
+  uart_ptr = UART_init();
+  uint8_t counter = 0;
+  uint8_t* receive_pkg[syzeof(_init_pkg)];
+  uint8_t success;
+  while(counter < RECEIVE_AMOUNT){
+    success = UART_getData(uart_ptr,receive_pkg,syzeof(_init_pkg));
+    if(success){
+      
+    }
+  }
   
 }
