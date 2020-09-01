befce398616915677a5512a5202035fe99efc66c
alessio123 <alessio.auddino@gmail.om>
2020-09-01 11:46:17 +0200

    AA: aggiunto modulo per messaggi atmega

--- common_lib/defs.h
+++ common_lib/defs.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef DEFS_H
+#define DEFS_H
 
 #define PKG_SIZE 2816
 
@@ -7,13 +8,16 @@
 #define INIT_HEADER "InitHeader"
 #define HEADER_SIZE 11              //valida per entrambi gli header
 
+#define SERVER_MSG_HEADER "ServerMsgHeader"
+#define SERVER_MSG_HEADER_SIZE 16
+
 //AA: struttura pacchetto (11 bytes = 2816 bits)
 typedef struct _data_pkg {
-  uint32_t checksum;                  // generato dal server
+  uint32_t checksum;                  // controllo integrità dati, generato dal server
   uint16_t data;                      // valore misurato
   uint8_t mask_pin;                   // pin da dove si legge il valore
   uint8_t cmd;                        // codice per la gestione dei pacchetti
-  int timestamp;                      // intero progressivo di mappatura dei pacchetti nell'allocatore
+  int timestamp;                      // intero che rappresenta l'epoca dei pacchetti
 } DataPkg;
 
 /*
@@ -37,3 +41,12 @@ typedef struct _init_pkg {
   int trigger;
 } InitPkg;
 
+
+//AA: messaggio di debug per atmega
+typedef struct server_msg_ {
+    char* header = SERVER_MSG_HEADER;
+    char* text;
+    size_t text_size;
+} server_msg;
+
+#endif
